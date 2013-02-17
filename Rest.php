131f71063128adc1a532d0e1eb6b5e08ab77660a
Wojciech Brüggemann <wojtek77@o2.pl>
2013-02-17 08:37:22 +0100

    adding APC cache for get() function

--- Rest.php
+++ Rest.php
@@ -8,10 +8,9 @@
  */
 class Rest
 {
-    const OUTPUT_RAW = 0;       // original unmodified output (as string)
-    const OUTPUT_RECOGNIZE = 1; // automatic recognition of output
-    const OUTPUT_JSON = 2;      // if JSON output as stdClass
-    const OUTPUT_XML = 3;       // if XML output as SimpleXMLElement
+    const TYPE_RAW = 0;
+    const TYPE_JSON = 1;
+    const TYPE_XML = 2;
     
     
     
@@ -21,19 +20,30 @@ class Rest
     private $request;
     
     /**
-     * @var int self-constant
+     * @var boolean
      */
-    private $output;
+    private $isRawOutput;
+    
+    
+    /**
+     * Whether APC cache is used (if it is set to FALSE, it certainly will not be used APC at all)
+     * @var boolean
+     */
+    private $isApc = true;
+    private $apcPrefix = __FILE__;          // prefix for variables in APC
+    private $apcLimitMemory = 0;            // in byte, 0 - no limit
+    private $apcLimitLive = 0;              // in second, 0 - no limit
+    private $apcIsCompression = false;      // if compression data
     
     
     
     /**
      * @param string $url   the target or base url
-     * @param int $output   a self-constant OUTPUT_...
+     * @param boolean $isRawOutput   if FALSE for JSON output is stdClass and for XML output is SimpleXMLElement
      */
-    public function __construct($url, $output=self::OUTPUT_RECOGNIZE)
+    public function __construct($url, $isRawOutput=false)
     {
-        $this->output = $output;
+        $this->isRawOutput = $isRawOutput;
         
         $this->request = new HttpRequest($url);
         
@@ -41,23 +51,46 @@ class Rest
             'redirect' => 10, // stop after 10 redirects
         );
         $this->request->setOptions($options);
+        
+        if ($this->isApc)
+            $this->isApc = extension_loaded('apc') && strstr($url, 'localhost/') === false;
     }
     
     
     /**
      * @param string $path
      * @param mixed $dataIn
-     * @return mixed
+     * @param boolean $isApc
+     * @return string|stdClass|SimpleXMLElement
      */
-    public function get($path='', $dataIn=null)
+    public function get($path='', $dataIn=null, $isApc=true)
     {
-        return $this->_fetch(HTTP_METH_GET, $path, 'setQueryData', $dataIn);
+        $request = clone $this->request;
+        
+        if ($isApc && $this->isApc)
+        {
+            $apcKey =
+                $this->apcPrefix
+                .$this->_finalUrl($request, $path)
+                .'?'.http_build_query((array)$dataIn, null, '&')
+                //.crc32(serialize($this))
+                ;
+            
+            $body = $this->_apcRead($apcKey);
+            
+            if ($body !== false)
+                return $body;
+        }
+        else
+            $apcKey = null;
+        
+        return $this->_fetch(HTTP_METH_GET, $path, 'setQueryData', $dataIn, $request, $apcKey);
     }
     
     /**
      * @param string $path
      * @param array $dataIn
-     * @return mixed
+     * @return string|stdClass|SimpleXMLElement
      */
     public function post($path='', array $dataIn=null)
     {
@@ -67,11 +100,11 @@ class Rest
     /**
      * @param string $path
      * @param string|array $dataIn
-     * @return mixed
+     * @return string|stdClass|SimpleXMLElement
      */
     public function put($path='', $dataIn=null)
     {
-        if (is_array($dataIn)) $dataIn = http_build_query ($dataIn, '', '&');
+        if (is_array($dataIn)) $dataIn = http_build_query($dataIn, null, '&');
         
         $request = clone $this->request;
         $request->setContentType('application/x-www-form-urlencoded');
@@ -81,7 +114,7 @@ class Rest
     
     /**
      * @param string $path
-     * @return mixed
+     * @return string|stdClass|SimpleXMLElement
      */
     public function delete($path='')
     {
@@ -91,9 +124,9 @@ class Rest
     
     
     /**
-     * @return mixed
+     * @return string|stdClass|SimpleXMLElement
      */
-    private function _fetch($method, $path, $callback, $dataIn, HttpRequest $request=null)
+    private function _fetch($method, $path, $callback, $dataIn, HttpRequest $request=null, $apcKey=null)
     {
         if (!isset($request)) $request = clone $this->request;
         $request->setMethod($method);
@@ -101,44 +134,47 @@ class Rest
         if (isset($callback)) $request->$callback($dataIn);
         $request->send();
         
-        if ($this->output === self::OUTPUT_RAW)
-        {
-            return $request->getResponseBody();
-        }
-        else
-        {
-            $body = $request->getResponseBody();
-            $bodyTrim = trim($body);
-            
-            /* JSON */
-            if ($this->output === self::OUTPUT_RECOGNIZE || $this->output === self::OUTPUT_JSON)
-            {
-                if (isset($bodyTrim{0}) && $bodyTrim{0} === '{')
-                {
-                    $return = json_decode($bodyTrim);
-                    if (json_last_error() === JSON_ERROR_NONE)
-                        return $return;
-                }
-            }
-            
-            /* XML */
-            if ($this->output === self::OUTPUT_RECOGNIZE || $this->output === self::OUTPUT_XML)
-            {
-                if (isset($bodyTrim{0}) && $bodyTrim{0} === '<')
-                {
-                    try
-                    {
-                        libxml_use_internal_errors(true);
-                        return new SimpleXMLElement($bodyTrim);
-                    }
-                    catch (Exception $e)
-                    {
-                        libxml_clear_errors();
-                    }
-                }
-            }
-            
+        $body = $request->getResponseBody();
+        if ($request->getResponseCode() !== 200)
             return $body;
+        
+        $type = strtolower($request->getResponseHeader('content-type'));
+        if (strpos($type, 'json') !== false)
+            $type = self::TYPE_JSON;
+        elseif (strpos($type, 'xml') !== false)
+            $type = self::TYPE_XML;
+        else
+            $type = self::TYPE_RAW;
+        
+        $data = (object) array(
+            'type' => $type,
+            'body' => $body,
+        );
+        
+        if (isset($apcKey))
+            $this->_apcWrite($apcKey, $data);
+        
+        return $this->_modifyOutput($data);
+    }
+    
+    /**
+     * @return string|stdClass|SimpleXMLElement
+     */
+    private function _modifyOutput(stdClass $data)
+    {
+        if ($this->isRawOutput)
+            return $data->body;
+        
+        switch ($data->type)
+        {
+            case self::TYPE_RAW:
+                return $data->body;
+            
+            case self::TYPE_JSON:
+                return json_decode($data->body);
+            
+            case self::TYPE_XML:
+                return new SimpleXMLElement($data->body);
         }
     }
     
@@ -152,5 +188,40 @@ class Rest
             $path = ltrim($path, '/');
         
         return $url.$path;
+    }
+    
+    /**
+     * @return boolean  if success
+     */
+    private function _apcWrite($key, stdClass $data)
+    {
+        $sum = 0;
+        if ($this->apcLimitMemory > 0)
+        {
+            foreach (new APCIterator('user', '/^'.preg_quote($this->apcPrefix).'/') as $v)
+            {
+                $sum += $v['mem_size'];
+            }
+        }
+        if ($sum > $this->apcLimitMemory) return false;
+        
+        if ($this->apcIsCompression)
+            $data = gzdeflate(serialize($data));
+        
+        return apc_store($key, $data, $this->apcLimitLive);
+    }
+    
+    /**
+     * @return string|stdClass|SimpleXMLElement|false  if failure return FALSE
+     */
+    private function _apcRead($key)
+    {
+        $data = apc_fetch($key);
+        if ($data === false) return false;
+        
+        if (is_string($data))
+            $data = unserialize(gzinflate($data));
+        
+        return $this->_modifyOutput($data);
     }
 }
